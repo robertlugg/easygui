4dc222a2707592ea256149f41cf500d216447f77
robertlugg <robert.lugg@gmail.com>
2014-12-20 01:12:59 -0800

    final? commit for 0.97 release
    Updated docs and got the api text generating properly.  This checkin
    also includes some 0.98 files, but frankly will likely delete them and
    start over after 0.97 is released.

--- easygui/__init__.py
+++ easygui/__init__.py
@@ -1,1 +1,3 @@
 from easygui import *
+pass
+__all__ = easygui.__all__
--- easygui_0.98/easygui.py
+++ easygui_0.98/easygui.py
@@ -403,6 +403,16 @@ def msgbox(msg="(Your message goes here)"
                      cancel_choice=ok_button)
 
 
+#
+# Reference: http://etutorials.org/Programming/Python+tutorial/Part+III+Python+Library+and+Extension+Modules/Chapter+16.+Tkinter+GUIs/16.9+Tkinter+Events/
+
+class ButtonBox(Frame):
+    def __init__(
+        self,
+        msg=None,
+        title=None
+        choices=None)
+
 #-------------------------------------------------------------------
 # buttonbox
 #-------------------------------------------------------------------
@@ -2567,24 +2577,15 @@ def _demo_fileopenbox():
 
 EASYGUI_ABOUT_INFORMATION = '''
 ========================================================================
-0.97(2014-12-20)
+0.97(2014-12-31)
 ========================================================================
-We are happy to release version 0.97 of easygui.  The intent of this release is to address some basic
-functionality issues as well as improve easygui in the ways people have asked.
-
-Robert Lugg (me) was searching for a GUI library for my python work.  I saw easygui and liked very much its
-paradigm.  Stephen Ferg, the creator and developer of easygui, graciously allowed me to start development
-back up.  With the help of Alexander Zawadzki, Horst Jens, and others I set a goal to release before the
-end of 2014.
-
-We rely on user feedback so please bring up problems, ideas, or just say how you are using easygui.
+Resolves some long standing bugs, a few cool enhancements, and a new set of documentation.
 
 BUG FIXES
 ---------
- * sourceforge #4: easygui docs contain bad references to easygui_pydoc.html
- * sourceforge #6: no index.html in docs download file.  Updated to sphinx which as autolinking.
- * sourceforge #8: unicode issues with file*box.  Fixed all that I knew how.
- * sourceforge #12: Cannot Exit with 'X'.  Now X and escape either return "cancel_button", if set, or None
+
+ * Converted documentation to Sphinx/reStructured text
+ * Some Sourceforge entered bugs resolved
 
 ENHANCEMENTS
 ------------
@@ -2597,30 +2598,11 @@ ENHANCEMENTS
    NOTE: We are still working on the exact syntax of these key mappings as Enter, space, and arrows are already being
          used.
  * Escape and the windows 'X' button always work in buttonboxes.  Those return None in that case.
- * sourceforge #9: let fileopenbox open multiple files.  Added optional argument 'multiple'
- * Location of dialogs on screen is preserved.  This isn't perfect yet, but now, at least, the dialogs don't
-   always reset to their default position!
- * added some, but not all of the bugs/enhancements developed by Robbie Brook:
-   http://all-you-need-is-tech.blogspot.com/2013/01/improving-easygui-for-python.html
-
-KNOWN ISSUES
-------------
- * In the documentation, there were previous references to issues when using the IDLE IDE.  I haven't
-   experienced those, but also didn't do anything to fix them, so they may still be there.  Please report
-   any problems and we'll try to address them
- * I am fairly new to contributing to open source, so I don't understand packaging, pypi, etc.  There
-   are likely problems as well as better ways to do things.  Again, I appreciate any help or guidance.
 
 Other Changes (that you likely don't care about)
-------------------------------------------------
  * Restructured loading of image files to try PIL first throw error if file doesn't exist.
  * Converted docs to sphinx with just a bit of doctest.  Most content was retained from the old site, so
-   there might be some redundancies still.  Please make any suggested improvements.
- * Set up a GitHub repository for development: https://github.com/robertlugg/easygui
-
-EasyGui is licensed under what is generally known as
-the "modified BSD license" (aka "revised BSD", "new BSD", "3-clause BSD").
-This license is GPL-compatible but less restrictive than GPL.
+   there might be some redundancies still.
 
 ========================================================================
 0.96(2010-08-29)
