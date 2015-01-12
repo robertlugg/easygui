279e70f2af9f0c0442bec4a17fd881e95b86e70a
robertlugg <robert.lugg@gmail.com>
2015-01-11 22:05:59 -0800

    fix to import updatable_text_box.py

--- easygui/boxes/updatable_text_box.py
+++ easygui/boxes/updatable_text_box.py
@@ -10,7 +10,13 @@ Version |release|
 import sys
 import string
 
-from . import utils as ut
+# Refs:
+#   https://www.python.org/dev/peps/pep-0366
+#   http://stackoverflow.com/questions/11536764/attempted-relative-import-in-non-package-even-with-init-py
+if __name__ == "__main__" and __package__ is None:
+    from os import path
+    sys.path.append(path.dirname(path.abspath(__file__)))
+import utils as ut
 
 if sys.hexversion >= 0x020600F0:
     runningPython26 = True
