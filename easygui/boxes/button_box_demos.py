09245efd0c89c75b58f1beb629df48975e1565de
Juan José Denis Corrales <jjdenis@gmail.com>
2016-10-01 17:59:59 +0200

    Split button_box in separate modules for better reading.

---
+++ easygui/boxes/button_box_demos.py
@@ -0,0 +1,66 @@
+import os
+
+try:
+    from . import button_box
+except (SystemError, ValueError, ImportError):
+    import buttonbox
+
+def demo_buttonbox_1():
+    print("hello from the demo")
+    value = buttonbox(
+        title="First demo",
+        msg="bonjour",
+        choices=["Button[1]", "Button[2]", "Button[3]"],
+        default_choice="Button[2]")
+    print("Return: {}".format(value))
+
+
+def demo_buttonbox_2():
+    package_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))  ;# My parent's directory
+    images = list()
+    images.append(os.path.join(package_dir, "python_and_check_logo.gif"))
+    images.append(os.path.join(package_dir, "zzzzz.gif"))
+    images.append(os.path.join(package_dir, "python_and_check_logo.png"))
+    images = [images, images, images, images, ]
+    value = buttonbox(
+        title="Second demo",
+        msg="Now is a good time to press buttons and show images",
+        choices=['ok', 'cancel'],
+        images=images)
+    print("Return: {}".format(value))
+
+
+def demo_buttonbox_3():
+    msg = "This demoes interfacing without a callback \nYou haven't pushed a button"
+    while True:
+        choice_selected = buttonbox(
+            title="This demoes interfacing without a callback",
+            msg=msg,
+            choices=["Button[1]", "Button[2]", "Button[3]"],
+            default_choice="Button[2]")
+
+        msg = "You have pushed button {} \nNotice the flicking".format(choice_selected)
+
+        if not choice_selected:
+            break
+
+
+def demo_buttonbox_4():
+
+    def actualize(box):
+        msg = "You have pushed button {} \nNotice the absence of flicking!!! ".format(box.choice_selected)
+        box.msg = msg
+
+    choice_selected = buttonbox(
+        title="This demoes interfacing with a callback",
+        msg="This demoes interfacing WITH a callback \nYou haven't pushed a button",
+        choices={"Button[1]":1, "Button[2]":2, "Button[3]":3},
+        default_choice="Button[2]",
+        callback=actualize)
+
+
+if __name__ == '__main__':
+    demo_buttonbox_1()
+    demo_buttonbox_2()
+    demo_buttonbox_3()
+    demo_buttonbox_4()
---
+++ easygui/boxes/button_box_validations.py
@@ -0,0 +1,81 @@
+import collections
+import sys
+
+try:
+    from .text_box import textbox
+except (SystemError, ValueError, ImportError):
+    import textbox
+
+
+class Validations(object):
+
+    def validate_images(self, image, images):
+        if image and images:
+            raise ValueError("Specify 'images' parameter only for buttonbox.")
+        if image:
+            images = image
+        return images
+
+    def images_to_matrix(self, img_filenames):
+        """
+        Create one or more images in the dialog.
+        :param img_filenames:
+        May be a filename (which will generate a single image), a list of filenames (which will generate
+        a row of images), or a list of list of filename (which will create a 2D array of buttons.
+        :return:
+        """
+
+        if img_filenames is None:
+            return
+        # Convert to a list of lists of filenames regardless of input
+        if self.is_string(img_filenames):
+            img_filenames = [[img_filenames, ], ]
+        elif self.is_sequence(img_filenames) and self.is_string(img_filenames[0]):
+            img_filenames = [img_filenames, ]
+        elif self.is_sequence(img_filenames) and self.is_sequence(img_filenames[0]) and self.is_string(img_filenames[0][0]):
+            pass
+        else:
+            raise ValueError("Incorrect images argument.")
+
+        return img_filenames
+
+    def convert_choices_to_dict(self, choices):
+        if isinstance(choices, collections.Mapping): # If it is dictionary-like
+            choices_dict = choices
+            choices_list = choices_dict.keys()
+        else:
+            # Convert into a dictionary of equal key and values
+            choices_list = list(choices)
+            choices_dict = {i: i for i in choices_list}
+        return choices_dict, choices_list
+
+
+    def to_string(self, something):
+        try:
+            basestring  # python 2
+        except NameError:
+            basestring = str  # Python 3
+
+        if isinstance(something, basestring):
+            return something
+        try:
+            text = "".join(something)  # convert a list or a tuple to a string
+        except:
+            textbox(
+                "Exception when trying to convert {} to text in self.textArea"
+                    .format(type(something)))
+            sys.exit(16)
+        return text
+
+    # REF: http://stackoverflow.com/questions/1835018/python-check-if-an-object-is-a-list-or-tuple-but-not-string
+    def is_sequence(self, arg):
+        return hasattr(arg, "__getitem__") or hasattr(arg, "__iter__")
+
+
+    def is_string(self, arg):
+        ret_val = None
+        try:
+            ret_val = isinstance(arg, basestring) #Python 2
+        except:
+            ret_val = isinstance(arg, str) #Python 3
+        return ret_val
