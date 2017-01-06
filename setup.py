8de1969ee789b0e29c5c28cd086a2e4f061559d6
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2017-01-06 20:42:58 +0000

    Update version to match latest release tag

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import distutils.core
 ## WARNING: Although the following import appears to do nothing, it is required for bdist_wheel to be recognized
 from setuptools import setup, find_packages
 
-version = "0.98.0"
-release = "0.98.0"
+version = "0.98.1"
+release = "0.98.1"
 
 desc = list()
 desc.append('EasyGUI is a module for very simple, very easy GUI programming in Python.  ')
